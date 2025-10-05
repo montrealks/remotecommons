59637a5e35a944a3ab282e661c34a24fc460b191
kris <kristifer.szabo@gmail.com>
2025-10-05 19:36:30 +0900

    .

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Remote Commons - Digital Nomad Visa Whitepaper</title>
-    <link rel="icon" type="image/png" href="/assets/logo.png">
+    <link rel="icon" type="image/png" href="/assets/favicon.png">
     <link rel="stylesheet" href="/assets/styles.css">
 </head>
 
