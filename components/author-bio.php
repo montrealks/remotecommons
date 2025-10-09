2115c8ec91d150d9dd4fe6a5ccea694abc8277a9
kris <kristifer.szabo@gmail.com>
2025-10-09 09:44:36 +0900

    .

--- components/author-bio.php
+++ components/author-bio.php
@@ -4,5 +4,17 @@
         <p class="lh-lg text-secondary mx-auto" style="max-width: 600px;">
             Lily Szabo is a digital nomad advocate with expertise in remote work visa policy and community building. Based in Chiang Mai, she runs CNXlocal and hosts the Amagi Village Podcast, part of a movement building regenerative villages for healthy families.
         </p>
+        <div class="mt-3">
+            <a href="mailto:lily@remotecommons.org" class="text-decoration-none me-3">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" class="text-dark">
+                    <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+                </svg>
+            </a>
+            <a href="https://www.linkedin.com/in/itslilyszabo" target="_blank" rel="noopener noreferrer" class="text-decoration-none">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" class="text-dark">
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                </svg>
+            </a>
+        </div>
     </div>
 </div>
